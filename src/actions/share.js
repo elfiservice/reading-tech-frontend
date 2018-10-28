f1533235df7fa57519516d2825a813f87d140f76
Armando Jr <elfiservice@hotmail.com>
2018-10-28 09:51:18 -0300

    feat: provided reducers and middleware config

--- src/actions/share.js
+++ src/actions/share.js
@@ -1,6 +1,26 @@
 import ApiAxios from '../util/apiconfig'
 
 import { receiveCategories } from './categories'
-import { receiveComments } from './comments'
+// import { receiveComments } from './comments'
 import { receivePosts } from './posts'
 
+function getCategories() {
+    return ApiAxios.get('/categories')
+}
+
+function getPosts() {
+    return ApiAxios.get('/posts')
+}
+
+export function handleInitialData() {
+    return (dispatch) => {
+        return ApiAxios.all([getCategories(), getPosts()])
+            .then(ApiAxios.spread(function (categories, posts) {
+                // Both requests are now complete
+                console.log(categories)
+                console.log(posts)
+                // dispatch(receiveCategories(categories))
+                // dispatch(receivePosts(posts))
+            }));
+    }
+} 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/font-awesome/css/font-awesome.min.css'
-
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 import App from './components/App';
+import middleware from './middleware'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, middleware)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
