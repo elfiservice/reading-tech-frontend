fdcff7547373977fd5bbac103eebf394e2210935
Armando Jr <elfiservice@hotmail.com>
2018-10-29 09:03:36 -0300

    refactore: change connect from CommentList to Post comtp

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
 import './CommentList.css'
 
-import { handleReceiveComments } from '../actions/comments'
 import Comment from './Comment'
 
 class CommentList extends Component {
-    componentDidMount() {
-        this.props.dispatch(handleReceiveComments(this.props.postId))
-    }
-
     render() {
         return (
             <div className="comment-list"> 
@@ -21,11 +15,5 @@ class CommentList extends Component {
     }
 }
 
-function mapStateToProps({ comments }, { postId }) {
-    return {
-        comments: Object.values(comments)
-            .filter( comment => comment.parentId === postId )
-    }
-}
 
-export default connect(mapStateToProps)(CommentList)
+export default CommentList
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,47 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import './Post.css'
+
+import { handleReceiveComments } from '../actions/comments'
 
 import { formatDate } from '../util/helpers'
 import CommentList from './CommentList'
 import AddComment from './AddComment'
 
-const Post = (props) => {
-    const { id, title, author, body, commentCount, timestamp, voteScore } = props.post   
-    return (
-        <article className="post">
-            <header className="inverse-color content">
-                <h3>{title}</h3>
-                <div className="author">
-                    by {author} 
-                    <span className="date"><i className="fa fa-calendar" aria-hidden="true"></i>{formatDate(timestamp)}</span>
-                </div> 
-            </header>
-            <div className="body content">
-                {body}
-            </div>
-            <div className="interactions inverse-color content">
-                <div className="votes">
-                    {voteScore} Votes
+class Post extends Component {
+    componentDidMount() {
+        this.props.dispatch(handleReceiveComments(this.props.id))
+    }
+    render() {
+        const { id, title, author, body, commentCount, timestamp, voteScore } = this.props.post
+        return (
+            <article className="post">
+                <header className="inverse-color content">
+                    <h3>{title}</h3>
+                    <div className="author">
+                        by {author} 
+                        <span className="date"><i className="fa fa-calendar" aria-hidden="true"></i>{formatDate(timestamp)}</span>
+                    </div> 
+                </header>
+                <div className="body content">
+                    {body}
                 </div>
-                <div className="comments-count">
-                    <div>{commentCount} comments</div>
+                <div className="interactions inverse-color content">
+                    <div className="votes">
+                        {voteScore} Votes
+                    </div>
+                    <div className="comments-count">
+                        <div>{commentCount} comments</div>
+                    </div>
                 </div>
-            </div>
-            <div className="comments-container">
-                <AddComment postId={id} />
-                <CommentList postId={id} />
-            </div>
+                <div className="comments-container">
+                    <AddComment postId={id} />
+                    <CommentList comments={this.props.comments} />
+                </div>
 
-        </article>
-    )
+            </article>
+        )
+    }
 }
 
-function mapStateToProps({ posts }, { id }) {
+function mapStateToProps({ posts, comments }, { id }) {
     const post = posts[id]
 
     return {
-        post
+        post,
+        comments: Object.values(comments)
+            .filter( comment => comment.parentId === id )
     }
 }
 
