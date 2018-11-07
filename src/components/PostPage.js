36f30e9a29fa489e671c3fcd98be833cbf1e464f
Armando Jr <elfiservice@hotmail.com>
2018-11-07 07:23:08 -0300

    feat: add edit btn in PostPage

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './PostPage.css'
 
 import Post from '../components/Post'
@@ -7,9 +8,20 @@ const PostPage = (props) => {
     const { id } = props.match.params
     return (
         <div className="post-page container">
-        <h2>Post Details</h2>
-        <Post id={id} />
-    </div>
+            <header className="header">
+                <h2>Post Details</h2>
+                <div className="edit-control">
+                    <Link className="btn btn-primary" to={`/edit-post/${id}`} >
+                        edit
+                    </Link>
+                </div>
+            </header>
+
+            <Post 
+                id={id}
+                edit
+                />
+        </div>
     )
 }
 
