6244066182555627330517d111651274df178212
Armando Jr <elfiservice@hotmail.com>
2018-11-12 07:33:33 -0300

    feat: provided input to edit each comment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -10,12 +10,59 @@ import VoteScore from './VoteScore'
 class Comment extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            editEnable: false,
+            body: ''
+        }
         this.scoreUpdateClick = this.scoreUpdateClick.bind(this);
+        this.toggleEditBtn = this.toggleEditBtn.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.saveEditedComment = this.saveEditedComment.bind(this);
+    }
+
+    componentDidMount() {
+        const { body } = this.props.comment
+        this.setState({ body })
+    }
+
+    handleInputChange(event) {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
     }
 
     scoreUpdateClick(option) {
         const { id } = this.props.comment
         this.props.dispatch(handlerVoteUpdate(id, option))  
+    }
+
+    toggleEditBtn() {
+        this.setState(( currentState ) => {
+            const newState = (currentState.editEnable ? false : true);
+            return { editEnable: newState }
+        })
+    }
+
+    renderCommentBody(body) {
+        if(this.state.editEnable) {
+            return (
+                <div className="edit-comment-form">
+                    <input name="body" value={this.state.body} onChange={this.handleInputChange} />
+                    <button className="btn btn-success btn-edit-comment" onClick={this.saveEditedComment}>
+                        <i className="fa fa-check" aria-hidden="true"></i>
+                    </button>
+                </div>
+                )
+        } else {
+            return <span>{body}</span>
+        }
+    }
+
+    saveEditedComment() {
+        const { id } = this.props.comment;
+        const { body } = this.state;
+
+        console.log(id, body);
+        
     }
 
     render() {
@@ -26,20 +73,19 @@ class Comment extends Component {
                     Commented by <span className="author">{author}</span> in <span>{formatDate(timestamp)}</span>
                 </div>
                 <div className="body">
-                    {body}
+                    {this.renderCommentBody(body)}
                 </div>
-                {/* criar botoes de Editar e Exclruir comentario */}
                 <div className="footer">
                     <VoteScore 
                         voteScore={voteScore}
                         scoreUpdate={this.scoreUpdateClick}
                     />
                     <div className="options">
-                        <button className="edit-comment btn" >
-                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                        <button className="edit-comment btn" onClick={this.toggleEditBtn} >
+                            <i className="fa fa-pencil" aria-hidden="true"></i>
                         </button>
                         <button className="delete-comment btn" >
-                            <i class="fa fa-trash-o" aria-hidden="true"></i>
+                            <i className="fa fa-trash-o" aria-hidden="true"></i>
                         </button>
                     </div>
                 </div>
