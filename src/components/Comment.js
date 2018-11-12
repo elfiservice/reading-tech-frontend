5d6f546762a988f94c42b5753678647ff693802a
Armando Jr <elfiservice@hotmail.com>
2018-11-12 06:58:54 -0300

    feat: provided edit and delete btns each Comment - and refactored Comment compt to a Class

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import './Comment.css'
 
@@ -7,26 +7,45 @@ import { handlerVoteUpdate } from '../actions/comments'
 import { formatDate } from '../util/helpers'
 import VoteScore from './VoteScore'
 
-const Comment = (props) => {
-    const { id, author, body, timestamp, voteScore } = props.comment
-    const scoreUpdateClick = (option) => {
-        props.dispatch(handlerVoteUpdate(id, option))       
+class Comment extends Component {
+    constructor(props) {
+        super(props)
+        this.scoreUpdateClick = this.scoreUpdateClick.bind(this);
     }
 
-    return (
-        <div className="comment"> 
-            <div className="apresetation">
-                Commented by <span className="author">{author}</span> in <span>{formatDate(timestamp)}</span>
+    scoreUpdateClick(option) {
+        const { id } = this.props.comment
+        this.props.dispatch(handlerVoteUpdate(id, option))  
+    }
+
+    render() {
+        const { id, author, body, timestamp, voteScore } = this.props.comment
+        return (
+            <div className="comment"> 
+                <div className="apresetation">
+                    Commented by <span className="author">{author}</span> in <span>{formatDate(timestamp)}</span>
+                </div>
+                <div className="body">
+                    {body}
+                </div>
+                {/* criar botoes de Editar e Exclruir comentario */}
+                <div className="footer">
+                    <VoteScore 
+                        voteScore={voteScore}
+                        scoreUpdate={this.scoreUpdateClick}
+                    />
+                    <div className="options">
+                        <button className="edit-comment btn" >
+                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                        </button>
+                        <button className="delete-comment btn" >
+                            <i class="fa fa-trash-o" aria-hidden="true"></i>
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div className="body">
-                {body}
-            </div>
-            <VoteScore 
-                voteScore={voteScore}
-                scoreUpdate={scoreUpdateClick}
-            />
-        </div>
-    )
+        )
+    }
 }
 
 export default connect()(Comment)
