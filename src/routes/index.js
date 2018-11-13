47b315bf4b990fc9176808cf75ab1c51efd80b3b
Armando Jr <elfiservice@hotmail.com>
2018-11-13 08:05:41 -0300

    refactore: routes to use Switch and redirect to home - when not Url Match

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
-import React, { Fragment } from 'react'
-import { Route } from 'react-router-dom'
+import React from 'react'
+import { Route, Redirect, Switch } from 'react-router-dom'
 
 import Home from '../components/Home'
 import NewPost from '../components/NewPost'
@@ -10,14 +10,15 @@ import Page404 from '../components/Page404'
 
 const Routes = (props) => {
     return (
-        <Fragment>
-            <Route path='/' exact component={Home} />
+        <Switch>
+            <Route exact path='/' component={Home} />
             <Route path='/new-post' exact component={NewPost} />
             <Route path='/category/:cat_name' exact component={Category} />
             <Route path='/post/:id' exact component={PostPage} />
             <Route path='/edit-post/:id' exact component={PostEdit} />
             <Route path='/404-page-not-found' exact component={Page404} />
-        </Fragment>
+            <Redirect from="*" to="/" />
+        </Switch>
     )
 }
 
